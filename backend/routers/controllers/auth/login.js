3551acab638653c3b7be2b7218a24395095ed7d1
RashedMigdady <migdadyrashed@gmail.com>
2021-10-31 16:03:15 +0200

    create Middleware for excute function with authentication

--- backend/routers/controllers/auth/login.js
+++ backend/routers/controllers/auth/login.js
@@ -1,27 +1,39 @@
 const connection = require("../../../DB/DB");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("dotenv").config();
+const login = (req, res) => {
+  const { email, password } = req.body;
 
-const login = (req , res)=>{
-    const {email , password } = req.body;
+  const query = `SELECT * FROM users WHERE email ='${email}'`;
 
-    const query = `SELECT * FROM users WHERE email ='${email}'`;
+  connection.query(query, async (err, result) => {
+    if (!result) {
+      res.status(500).json({ result: "Email is Not found!" });
+    }
 
-    connection.query(query ,async (err , result)=>{
-        if (!result)
-        {res.status(500).json({result :"Email is Not found!"})}
-        
+    try {
+      const valid = await bcrypt.compare(password, result[0].password);
 
-        const valid =await bcrypt.compare(password ,result[0].password)
+      if (!valid) {
+        res.status(200).json({ message: "The password incorrect!" });
+      } else {
+          const payload ={
+              userId : result[0].id,
+              email : result[0].email,
+              role: result[0].role,
+          }
+          
+          const options = {
+            expiresIn:"100h"
+          }
+          const token = jwt.sign(payload , process.env.SECRET , options)
+        res.status(200).json({ message: "you are logged!" ,token:token });
+      }
+    } catch (error) {
+      res.status(404).json({ Error: error });
+    }
+  });
+};
 
-        if (!valid)
-        {
-            res.status(200).json({message:"The password incorrect!"})
-        }else
-        {
-            res.status(200).json({message:"you are logged!"})
-        }
-    })
-}
-
-
-module.exports = {login}
+module.exports = { login };
---
+++ backend/routers/controllers/users.js
@@ -0,0 +1,13 @@
+const connection = require("../../DB/DB");
+
+const getAllUsers = (req , res)=>{
+    const query = `SELECT * FROM users`;
+
+    connection.query(query , (err , users)=>{
+        if (err) return res.status(404).json({Error:err})
+
+        res.status(200).json(users);
+    })
+}
+
+module.exports = getAllUsers;
