c5b21264faded727db00d12d43d00be1bd018e23
RashedMigdady <migdadyrashed@gmail.com>
2021-10-31 08:57:15 +0200

    configration server and built structure backend files

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(express.json());
+
+app.use(cors());
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running On PORT= ${PORT}`);
+});
